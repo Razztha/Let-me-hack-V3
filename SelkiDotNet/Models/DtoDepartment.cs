9743fe8ed414c748a78e93e72073ea8437586665
DESKTOP-UOU760Q\Rasitha <razztha111@gmail.com>
2018-02-16 01:26:27 +0530

    Implement POST /departments

---
+++ SelkiDotNet/Models/DtoDepartment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelkiDotNet.Models
+{
+    public class DtoDepartment
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Nullable<int> faculty_id { get; set; }
+    }
+}
