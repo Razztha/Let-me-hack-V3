8cdeff0f667e3474bfe114841746671cf92e6136
sameeranihathe <sameeranihathe@gmail.com>
2018-02-18 15:04:28 +0530

    Implement degrees and department controller changed

---
+++ SelkiDotNet/Models/SuccessMsg/DtoDegreeGetSuccessMsg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelkiDotNet.Models.SuccessMsg
+{
+    public class DtoDegreeGetSuccessMsg
+    {
+        public string self { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+    public class degreelist
+    {
+        public List<DtoDegreeGetSuccessMsg> degrees { get; set; }
+    }
+}
